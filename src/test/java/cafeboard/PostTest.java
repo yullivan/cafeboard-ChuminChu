fbc47e4d667c5f8791b6890a107c3c306618097c
chumin-yeong <chu35my@gmail.com>
2025-01-10 17:57:34 +0900

    fix: 예외처리, 하드코딩 변경

--- src/test/java/cafeboard/PostTest.java
+++ src/test/java/cafeboard/PostTest.java
@@ -1,6 +1,7 @@
 package cafeboard;
 
 import cafeboard.board.BoardRequest;
+import cafeboard.board.BoardResponse;
 import cafeboard.post.PostRequest;
 import cafeboard.post.PostResponse;
 import io.restassured.RestAssured;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostTest {
@@ -23,30 +26,59 @@ public class PostTest {
 
     @Test
     void 게시글생성테스트() {
-        RestAssured
+        BoardResponse 게시판1 = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new BoardRequest("자유게시판"))
+                .body(new BoardRequest("게시판1"))
                 .when()
                 .post("/boards")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .as(BoardResponse.class);
+
+        PostResponse 게시글 = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new PostRequest("안녕하세요", "인사", 게시판1.id()))
+                .when()
+                .post("/posts")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(PostResponse.class);
+
+        assertThat(게시글.content()).isNotNull();
+        assertThat(게시글.title()).isNotNull();
+    }
+    @Test
+    void 게시글전체조회테스트() {
+        BoardResponse 자유게시판 = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new BoardRequest("게시판2"))
+                .when()
+                .post("/boards")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(BoardResponse.class);
+
+        PostResponse 게시글 = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new PostRequest("안녕하세요", "인사", 자유게시판.id()))
+                .when()
+                .post("/posts")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(PostResponse.class);
 
         RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new PostRequest("안녕하세요", "인사", (long) 1))
-                .when()
-                .post("/posts")
-                .then().log().all()
-                .statusCode(200);
-    }
-    @Test
-    void 게시글전체조회테스트() {
-        RestAssured
-                .given().log().all()
-                .contentType(ContentType.JSON)
-                .body(new PostRequest("안녕하세요", "인사", (long) 1))
+                .body(new PostRequest("안녕하세요", "인사", 자유게시판.id()))
                 .when()
                 .get("/posts")
                 .then().log().all()
@@ -55,27 +87,31 @@ public class PostTest {
 
     @Test
     void 특정게시글조회테스트() {
-        RestAssured
+
+        BoardResponse 자유게시판 = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new BoardRequest("자유게시판"))
+                .body(new BoardRequest("게시판3"))
                 .when()
                 .post("/boards")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract().as(BoardResponse.class);
 
-        RestAssured
+        PostResponse postResponse = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new PostRequest("안녕하세요", "인사", (long) 1))
+                .body(new PostRequest("안녕하세요", "인사", 자유게시판.id()))
                 .when()
                 .post("/posts")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .as(PostResponse.class);
 
         RestAssured
                 .given().log().all()
-                .pathParam("postId", "1")
+                .pathParam("postId", postResponse.id())
                 .when()
                 .get("/posts/{postId}")
                 .then().log().all()
@@ -84,28 +120,32 @@ public class PostTest {
 
     @Test
     void 게시글수정() {
-        RestAssured
+        BoardResponse 자유게시판 = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new BoardRequest("자유게시판"))
+                .body(new BoardRequest("게시판4"))
                 .when()
                 .post("/boards")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .as(BoardResponse.class);
 
-        RestAssured
+        PostResponse postResponse = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new PostRequest("안녕하세요", "인사", (long) 1))
+                .body(new PostRequest("안녕하세요", "인사", 자유게시판.id()))
                 .when()
                 .post("/posts")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .as(PostResponse.class);
 
         RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new PostRequest("아효 힘들어", "제목이다",(long) 1))
+                .body(new PostRequest("아효 힘들어", "제목이다", postResponse.id()))
                 .pathParam("postId", "2")
                 .when()
                 .put("/posts/{postId}")
@@ -116,28 +156,31 @@ public class PostTest {
 
     @Test
     void 게시글삭제테스트() {
-        RestAssured
+        BoardResponse 자유게시판 = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new BoardRequest("자유게시판"))
+                .body(new BoardRequest("게시판5"))
                 .when()
                 .post("/boards")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract().as(BoardResponse.class);
 
-        RestAssured
+        PostResponse postResponse = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new PostRequest("안녕하세요", "인사", (long) 1))
+                .body(new PostRequest("안녕하세요", "인사", 자유게시판.id()))
                 .when()
                 .post("/posts")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .as(PostResponse.class);
 
         RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .pathParam("postId", "1")
+                .pathParam("postId", postResponse.id())
                 .when()
                 .delete("/posts/{postId}")
                 .then().log().all()
